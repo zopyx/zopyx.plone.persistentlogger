ff2b728555e95179fe1f29351d4259b46e018a78
zopyx <yet@gmx.de>
2015-08-31 11:37:48 +0200

    updated

---
+++ zopyx/plone/persistentlogger/browser/logger.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# zopyx.plone.persistentlogger
+# (C) 2015,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from zopyx.plone.persistentlogger.logger import IPersistentLogger
+
+
+class Logging(BrowserView):
+
+    template = ViewPageTemplateFile('logger.pt')
+
+    def entries(self):
+        return IPersistentLogger(self.context).entries
+
+    def log_clear(self):
+        """ Clear connector persistent log """
+        IPersistentLogger(self.context).clear()
+        msg = u'Log entries cleared'
+        self.context.plone_utils.addPortalMessage(msg)
+        return self.request.response.redirect(
+            '{}/connector-log'.format(self.context.absolute_url()))
+
+    def __call__(self):
+        return self.template()
