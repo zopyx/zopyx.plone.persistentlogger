2d24431e0b985de59dac04be4656e98617c87832
zopyx <yet@gmx.de>
2015-08-31 13:52:50 +0200

    updated

---
+++ build_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PATH=\
+/opt/buildout.python/bin:\
+$PATH:
+
+if [[ "$1" = "plone-4.3" ]]
+then
+    config=buildout-plone-4.3.cfg
+fi
+
+if [[ "$1" = "plone-5.0" ]]
+then
+    config=buildout-plone-5.0.cfg
+fi
+
+
+virtualenv-2.7 .
+bin/pip install -U setuptools==7.0  
+bin/python bootstrap.py -c $config --setuptools-version 7.0 --version 2.2.5
+bin/buildout -c $config
+bin/test -s zopyx.plone.persistentlogger
