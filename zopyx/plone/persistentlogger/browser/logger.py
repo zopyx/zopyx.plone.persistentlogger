c8f1b7975fae3fbb2ecb87d81e51d31003919c3b
Andreas JungÂ <info@zopyx.com>
2016-04-20 04:45:57 +0200

    cleanup

--- zopyx/plone/persistentlogger/browser/logger.py
+++ zopyx/plone/persistentlogger/browser/logger.py
@@ -5,8 +5,12 @@
 # (C) 2015,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
 ################################################################
 
+
+from zope.interface import alsoProvides
+
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.protect.interfaces import IDisableCSRFProtection
 
 from zopyx.plone.persistentlogger.logger import IPersistentLogger
 
@@ -16,10 +20,12 @@ class Logging(BrowserView):
     template = ViewPageTemplateFile('logger.pt')
 
     def entries(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
         return IPersistentLogger(self.context).entries
 
     def log_clear(self):
         """ Clear persistent log """
+        alsoProvides(self.request, IDisableCSRFProtection)
         IPersistentLogger(self.context).clear()
         msg = u'Log entries cleared'
         self.context.plone_utils.addPortalMessage(msg)
