003739a6f0861b1df34ad10f7d873f823c3b2180
Andreas JungÂ <info@zopyx.com>
2019-11-15 15:51:10 +0100

    updated

--- build_test.sh
+++ build_test.sh
@@ -4,19 +4,9 @@ export PATH=\
 /opt/buildout.python/bin:\
 $PATH:
 
-if [[ "$1" = "plone-4.3" ]]
+if [[ "$1" = "plone-5.2" ]]
 then
-    config=buildout-plone-4.3.cfg
-fi
-
-if [[ "$1" = "plone-5.0" ]]
-then
-    config=buildout-plone-5.0.cfg
-fi
-
-if [[ "$1" = "plone-5.1" ]]
-then
-    config=buildout-plone-5.1.cfg
+    config=buildout.cfg
 fi
 
 
