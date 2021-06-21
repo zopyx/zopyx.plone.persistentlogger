b1138cd25964432fe755a01d5e4d2f376ede7331
Andreas Jung <info@zopyx.com>
2021-06-21 15:26:07 +0200

    minor cleanup

--- zopyx/plone/persistentlogger/file_logger.py
+++ zopyx/plone/persistentlogger/file_logger.py
@@ -17,9 +17,11 @@ log_home.mkdir(parents=True, exist_ok=True)
 
 LOG_ROOT = log_home
 DEFAULT_FMT = "{time:YYYYMMDDTHH:mm:ss} {level:8s} {message}"
+DEFAULT_LEVEL = "INFO"
 
 
 def new_logger():
+    """ Create a new loguru.Logger instance """
     return _Logger(_Core(), None, 0, False, False, False, False, True, None, {})
 
 
@@ -30,20 +32,19 @@ def get_logger(
         retention="3 months",
         log_as_json=False,
         fmt=DEFAULT_FMT,
-        log_root=LOG_ROOT
+        log_root=LOG_ROOT,
+        level=DEFAULT_LEVEL
         ):
 
     LOG = new_logger()
     if log_stdout:
-        LOG.add(sys.stdout, format=fmt)
+        LOG.add(sys.stdout, format=fmt, level=level)
     if prefix:
         log_name = log_root / (prefix + ".log")
-        LOG.add(log_name, rotation=rotation, format=fmt, retention=retention)
+        LOG.add(log_name, rotation=rotation, format=fmt, retention=retention, level=level)
         LOG.info(f"Logfile added: {log_name}")
     if log_as_json:
         log_name = log_root / (prefix + ".json")
-        LOG.add(log_name, rotation=rotation, serialize=True, format=fmt, retention=retention)
+        LOG.add(log_name, rotation=rotation, serialize=True, format=fmt, retention=retention, level=level)
         LOG.info(f"Logfile added: {log_name}")
     return LOG
-
-
